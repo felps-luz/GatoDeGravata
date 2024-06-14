3bfe37d5291445c03e7136b6d8c275ff91dbc6ff
felps-luz <133365350+felps-luz@users.noreply.github.com>
2024-06-14 10:34:07 -0300

    Add files via upload

---
+++ appp.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import style from './styles';
+
+const App = () =>{
+    return (
+        <view style={style.container}>
+            <text style={StyleSheet.text}>Olá, mundo!</text>
+        </view>
+    );
+};
+
+export default App;
